ef8dd0d2d9e7aa64114388d1f83bd42aaa94093e
Anurag Nema <66417880+Axe-Pearl@users.noreply.github.com>
2022-03-05 19:30:47 +0530

    Leetcode 1108. Defanging an IP Address
    https://leetcode.com/problems/defanging-an-ip-address/

---
+++ Day12/(1108)Defanging_an_IP_Address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res = "";
+        for(int i = 0;i<address.size();i++){
+            if(address[i] == '.'){
+                res+='[';
+                res+=address[i];
+                res+=']';
+            }
+            else{
+                 res+=address[i];
+            }
+        }
+        return res;
+    }
+};
